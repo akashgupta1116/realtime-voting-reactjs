fb6a9aa610cc9c1969863de7c6d19ad83d80ea0d
akashgupta1116 <yoakash6@gmail.com>
2020-10-13 08:55:16 +0530

    formatting

--- voting-app/src/components/Main.js
+++ voting-app/src/components/Main.js
@@ -6,9 +6,6 @@ const Main = () => (
     <h1 className="main-heading">Voting app</h1>
     <h5>Hi Folks!! Welcome to the Realtime Voting App</h5>
     <div className="main-btn-section">
-      {/* <Link to="/dashboard" target="_blank" className="show-stats-btn">
-        Dashboard
-      </Link> */}
       <Link to="/voting" target="_blank" className="show-stats-btn">
         Voting App
       </Link>
